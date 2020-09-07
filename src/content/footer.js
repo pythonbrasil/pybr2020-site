9e9c1fabf9526027c5946be726eb7800efb5fa13
Camila Maia <cmaiacd@gmail.com>
2020-09-07 15:42:47 -0300

    Initial structured copied from 2019 website
    Remove city guide section already, since the event is online

---
+++ src/content/footer.js
@@ -0,0 +1,28 @@
+module.exports = {
+    nav: [
+        {
+            title: 'Sobre',
+            items: [
+                { text: 'Local', link: 'https://goo.gl/maps/m7ttyu5CCnBjobmD7' },
+                { text: 'Keynotes', link: '#section-keynote' },
+                { text: 'Evento', link: '#section-about' },
+                // { text: 'Programação', link: '#' },
+                { text: 'Código de Conduta', link: 'https://python.org.br/cdc/' },
+            ]
+        },
+        {
+            title: 'Patrocínio',
+            items: [
+                { text: 'Quero Patrocinar', link: '#section-sponsor' },
+                { text: 'Patrocinadores', link: '#section-sponsors' },
+            ]
+        },
+    ],
+    social: {
+        facebook: { link: 'https://www.facebook.com/pythonbrasil/', logo: '/images/facebook.svg' },
+        twitter: { link: 'https://twitter.com/pythonbrasil', logo: '/images/twitter.svg' },
+        instagram: { link: 'https://www.instagram.com/pythonbrasil/', logo: '/images/instagram.svg' },
+        apybr: { link: 'https://python.org.br/', logo: '/images/pylogo.svg' }
+    },
+    buyTicketLink: 'https://pythonbrasil2019.eventbrite.com.br'
+}
