f4de3fb3b8e58e84411397ad74c839ee476d1480
Camila Maia <cmaiacd@gmail.com>
2020-09-07 15:42:47 -0300

    Initial structured copied from 2019 website
    Remove city guide section already, since the event is online

---
+++ src/content/keynotes/felipemorais.md
@@ -0,0 +1,11 @@
+---
+photo: felipemorais.jpg
+linkedin: https://www.linkedin.com/in/felipe-de-morais
+twitter: https://www.twitter.com/felipedemorais_
+publish: true
+---
+
+### Felipe de Morais
+#### Comunidade AfroPython
+
+Felipe de Morais é um Desenvolvedor de Software envolvido há alguns anos com a comunidade Python. Entre as contribuições mais relevantes para a Comunidade estão o AfroPython, iniciativa para empoderar pessoas negras através da tecnologia e colaborações em projetos abertos como a Operação Serenata de Amor. Tudo que ele faz são manifestações de seus 2 valores mais básicos: Ajudar pessoas e aprender com e de pessoas incríveis.
---
+++ src/content/sponsors.js
@@ -0,0 +1,61 @@
+const Entity = (name, photo, link) => ({
+    name, link,
+    photo: `/assets/images/entities/${photo}`
+});
+
+module.exports = {
+    sponsors: {
+        title: 'Patrocinadores',
+        categories: [
+            {
+                type: 'diamante',
+                items: [
+                    Entity('GitHub', 'GitHub_Logo.png', 'https://github.com'),
+                ],
+            },
+            {
+                type: 'platina',
+                items: [
+                    Entity('PSF', 'PSF-Logo-Narrow-Shapes.svg', 'https://www.python.org/psf'),
+                    Entity('GrupoZAP', 'grupozap.png', 'https://www.grupozap.com'),
+                ],
+            },
+            {
+                type: 'ouro',
+                items: [
+                    Entity('Loadsmart', 'loadsmart-logo-horizontal.png', 'https://www.loadsmart.com'),
+                    Entity('olist', 'olist.png', 'https://olist.com'),
+                    Entity('Stoodi', 'stoodi.png', 'https://www.stoodi.com.br/'),
+                    Entity('Globo.com', 'logo_globo_com.svg', 'https://www.globo.com/'),
+                ],
+            },
+            {
+                type: 'prata',
+                items: [
+                    Entity('Evolux', 'logo-evolux.svg', 'https://www.evolux.net.br'),
+                    Entity('Lab Codes', 'labcodes.png', 'https://labcodes.com.br/'),
+                    Entity('WhatsGood', 'whatsgood.png', 'https://whatsgood.com.br'),
+                ],
+            },
+            {
+                type: 'bronze',
+                items: [
+                    Entity('iDtrust', 'logo-idtrust.png', 'https://www.idtrust.com.br'),
+                ],
+            },
+        ]
+    },
+    partners: [
+      {
+        title: 'Parceiros',
+        items: [
+          Entity('associação python brasil', 'apyb.png', 'https://python.org.br/'),
+          Entity('USP', 'usp.png', 'https://www5.usp.br/'),
+          Entity('Estácio de Sá', 'estacio.png', 'http://portal.estacio.br')
+        ]
+      }
+    ],
+    organization: [
+        Entity('FizzFuzz', 'placeholder.png', '/sponsor-test'),
+    ]
+}
